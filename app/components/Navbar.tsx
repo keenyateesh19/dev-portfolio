7f15a0b3832275bd3a3972996bfcf4807f81e030
Yateesh S <keenyateesh@gmail.com>
2025-12-04 13:00:02 +0530

    Add closeMenu functionality to NavLink components in NavBar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,12 +7,15 @@ const NavBar = () => {
   const base = "transition hover:text-blue-400";
   const active = "text-blue-400 font-semibold";
   const [menuOpen, setMenuOpen] = useState(false);
+  const closeMenu = () => setMenuOpen(false);
+
   return (
     <nav className="bg-gray-800 border-b border-gray-700 shadow-md sticky top-0 z-50">
       <div className="max-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
         <NavLink
           to="/"
           className="flex items-center gap-2 text-lg font-bold text-blue-300"
+          onClick={closeMenu}
         >
           <FaLaptopCode className="text-blue-400 text-xl" />
           <span>YATEESH.TECH</span>
@@ -69,30 +72,35 @@ const NavBar = () => {
               <NavLink
                 className={({ isActive }) => (isActive ? active : base)}
                 to="/"
+                onClick={closeMenu}
               >
                 Home
               </NavLink>
               <NavLink
                 className={({ isActive }) => (isActive ? active : base)}
                 to="/projects"
+                onClick={closeMenu}
               >
                 Projects
               </NavLink>
               <NavLink
                 className={({ isActive }) => (isActive ? active : base)}
                 to="/blogs"
+                onClick={closeMenu}
               >
                 Blogs
               </NavLink>
               <NavLink
                 className={({ isActive }) => (isActive ? active : base)}
                 to="/about"
+                onClick={closeMenu}
               >
                 About
               </NavLink>
               <NavLink
                 className={({ isActive }) => (isActive ? active : base)}
                 to="/contact"
+                onClick={closeMenu}
               >
                 Contact
               </NavLink>
