cf31b23765bdf59b2e3a8c116490c654457ce379
Yateesh S <keenyateesh@gmail.com>
2025-12-04 15:51:38 +0530

    feat: add project types and sample data for projects
    - Created a new TypeScript type for Project in `app/types.ts`.
    - Added a JSON file `data/db.json` containing sample project data.
    - Updated `package.json` to include a script for running json-server.
    - Added images for projects and a placeholder image in the public directory.

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,5 +1,28 @@
-const projectsPage = () => {
-    return ( <h1 className="text-3xl text-white font-bold mb-8">🚀 Projects</h1> );
+import ProjectCard from "~/components/ProjectCard";
+import type { Route } from "./+types/index";
+import type { Project } from "~/types";
+
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
+  const res = await fetch("http://localhost:8000/projects");
+  const data = await res.json();
+
+  return { projects: data };
 }
- 
+
+const projectsPage = ({ loaderData }: Route.ComponentProps) => {
+  const { projects } = loaderData as { projects: Project[] };
+  return (
+    <>
+      <h2 className="text-3xl text-white font-bold mb-8">🚀 Projects</h2>
+      <div className="grid gap-6 sm:grid-cols-2">
+        {projects.map((project) => (
+          <ProjectCard project={project} key={project.id}/>
+        ))}
+      </div>
+    </>
+  );
+};
+
 export default projectsPage;
