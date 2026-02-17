e984084a4389855fb2dc757db22e694f8b3b2a6a
Yateesh S <keenyateesh@gmail.com>
2026-02-17 14:54:28 +0530

    feat: implement LetterGlitch component and update font styles in Hero section

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,23 +1,25 @@
 import { Link } from "react-router";
-
+import LetterGlitch from "./LetterGlitch";
 
 const Hero = () => {
   return (
-    <header className="text-center py-20 bg-gray-900 text-white transition-colors duration-300">
-      <h2 className="text-4xl font-bold mb-4">Hey there, I'm Yateesh 👋</h2>
-      <p className="text-lg text-gray-400 max-w-2xl mx-auto mb-6">
-        I'm a passionate developer who loves building beautiful and functional
-        web applications. Welcome to my portfolio!
-      </p>
-      <div className="flex justify-center gap-4">
-        <Link
-          to="/projects"
-          className="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700 transition"
-        >
-          View Projects
-        </Link>
-        <Link
-            to='/contact' className="border border-blue-500 text-blue-400 px-6 py-2 rounded hover:bg-blue-500 hover:text-white">Contact Me</Link>
+    <header className="h-screen bg-amber-100 w-full relative">
+      <div className="absolute w-full h-svh z-0">
+        <LetterGlitch
+          glitchColors={["#2b4539", "#61dca3", "#61b3dc"]}
+          glitchSpeed={15}
+          centerVignette={true}
+          characters="ಅಆಇಈಉಊಋಎಏಐಒಓಔಕಖಗಘಚಜಟಡತದನಪಬಮಯರಲವಶಸಹಳ೦೧೨೩೪೫೬೭೮೯ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$&*()-_+=/[]{};:<>.,"
+        />
+      </div>
+      <div className="h-screen absolute z-1 w-full flex flex-col justify-center items-center text-center px-6 gap-4">
+        <h1 className="font-display lg:max-w-[65%]">
+           Helping founder turn ideas into seamless <span className="bg-linear-to-r from-cyan-400 via-blue-500 to-purple-600 bg-clip-text text-transparent">Digital Experiences</span>
+        </h1>
+        <p>
+          Building meaningful things for the web. <br />With a focus on performance
+          and real-world impact.
+        </p>
       </div>
     </header>
   );
